f3144484c139c4c7b60cc9a0a0b5655d37121c73
laurabalasso <laurabalasso96@gmail.com>
2019-12-07 15:01:51 +0100

    pseudo relevance feedback implemented

--- BIM.py
+++ BIM.py
@@ -4,6 +4,10 @@ import re
 import numpy as np
 import time
 import sys
+from copy import deepcopy
+
+
+# ## Import the data ad create the inverted index
 
 
 
@@ -31,8 +35,7 @@ def import_dataset():
 def remove_stop_words(corpus):
    '''
    This function removes from the corpus all the stop words present in the file TIME.STP
-   '''    
-   
+   '''
    stop_w = [line.rstrip('\n') for line in open('TIME.STP')]
    stop_w=list(filter(None, stop_w))
    for i in range(0,len(corpus)):
@@ -58,31 +61,31 @@ def make_inverted_index(corpus):
 
 
 def posting_lists_union(pl1, pl2):
-    """
-    Returns a new posting list resulting from the union of this
-    one and the one passed as argument.
-    """
-    pl1 = sorted(list(pl1))
-    pl2 = sorted(list(pl2))
-    union = []
-    i = 0
-    j = 0
-    while (i < len(pl1) and j < len(pl2)):
-        if (pl1[i] == pl2[j]):
-            union.append(pl1[i])
-            i += 1
-            j += 1
-        elif (pl1[i] < pl2[j]):
-            union.append(pl1[i])
-            i += 1
-        else:
-            union.append(pl2[j])
-            j += 1
-    for k in range(i, len(pl1)):
-        union.append(pl1[k])
-    for k in range(j, len(pl2)):
-        union.append(pl2[k])
-    return union
+        """
+        Returns a new posting list resulting from the union of this
+        one and the one passed as argument.
+        """
+        pl1 = sorted(list(pl1))
+        pl2 = sorted(list(pl2))
+        union = []
+        i = 0
+        j = 0
+        while (i < len(pl1) and j < len(pl2)):
+            if (pl1[i] == pl2[j]):
+                union.append(pl1[i])
+                i += 1
+                j += 1
+            elif (pl1[i] < pl2[j]):
+                union.append(pl1[i])
+                i += 1
+            else:
+                union.append(pl2[j])
+                j += 1
+        for k in range(i, len(pl1)):
+            union.append(pl1[k])
+        for k in range(j, len(pl2)):
+            union.append(pl2[k])
+        return union
 
 
 # ## Precomputing weights
@@ -119,9 +122,9 @@ class BIM():
     '''
     
     def __init__(self, corpus):
-
+        self.original_corpus = deepcopy(corpus)
         self.articles = corpus
-        self.index = make_inverted_index(corpus)
+        self.index = make_inverted_index(self.articles)
         self.weights = RSV_weights(self.articles, self.index)
         self.ranked = []
         self.query_text = ''
@@ -159,24 +162,59 @@ class BIM():
         return self.ranked
     
     
+    def recompute_weights(self, relevant_idx, query):
+        '''
+        Auxiliary function for the relevance feedback.
+        Recomputes the weights, only for the terms in the query
+        based on a set of relevant documents.
+        '''
+        
+        relevant_docs = []
+        for idx in relevant_idx:
+            doc_id = self.ranked[idx-1][0]
+            relevant_docs.append(self.articles[doc_id])
+        
+        N = len(self.articles)
+        N_rel = len(relevant_idx)
+        
+        for term in query:
+            vri = 0
+            for doc in relevant_docs:
+                if term in doc:
+                    vri += 1
+            p = (vri + 0.5) /( N_rel + 1)
+            u = (DF(term, self.index) - vri + 0.5) / (N - N_rel +1) 
+            self.weights[term] = log((1-u)/u) + log(p/(1-p))
+            
+    
     def answer_query(self, query_text):
         '''
         Function to answer a free text query. Shows the first 30 words of the
-        15 most relevant documents.
+        15 most relevant documents. 
+        Also implements the pseudo relevance feedback with k = 5
         '''
         
         self.query_text = query_text
         query =  query_text.upper().split()
         ranking = self.ranking(query)
         
+        ## pseudo relevance feedback 
+        i = 0
+        new_ranking=[]
+        while i<10 and ranking != new_ranking:
+            self.recompute_weights([1,2,3,4,5], query)
+            new_ranking = self.ranking(query)
+            i+=1
+        
+        ranking = new_ranking
+        
         for i in range(0, 15):
-            article = self.articles[ranking[i][0]]
+            article = self.original_corpus[ranking[i][0]]
             if (len(article) > 30):
                 article = article[0:30]
             text = " ".join(article)
             print(f"Article {i + 1}, score: {ranking[i][1]}")
             print(text, '\n')
-              
             
             
     def relevance_feedback(self, *args):
@@ -192,25 +230,14 @@ class BIM():
         if(isinstance(relevant_idx[0], list)):
             relevant_idx = relevant_idx[0]
         
-        relevant_docs = []
-        for idx in relevant_idx:
-            doc_id = self.ranked[idx-1][0]
-            relevant_docs.append(self.articles[doc_id])
-        
-        N = len(self.articles)
-        N_rel = len(relevant_idx)
-
-        for term in self.index.keys():
-            vri = 0
-            for doc in relevant_docs:
-                if term in doc:
-                    vri += 1
-            p = (vri + 0.5) /( N_rel + 1)
-            u = (DF(term, self.index) - vri + 0.5) / (N - N_rel +1)
-            self.weights[term] = log((1-u)/u) + log(p/(1-p))
+        query = self.query_text.upper().split()
+        self.recompute_weights(relevant_idx,query)
         
         self.answer_query(self.query_text)
         
+     
+    
+
 
 
 # Example of usage
@@ -219,7 +246,4 @@ class BIM():
 # bim  = BIM(articles)
 # bim.answer_query('Italy and Great Britain fight the enemy')
 # bim.relevance_feedback(5,6,8)
-
-
-
 
